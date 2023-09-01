a6a6374985311e34d9161f56939f663081dd77a6
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-08-31 18:47:06 -0600

    Add files via upload

---
+++ 6-2/prjs6_2.js
@@ -0,0 +1,6 @@
+// document.getElementById("p").style.color = "blue";
+// document.getElementById("p").style.fontSize = "xx-large"
+
+document.write("<p style='color: blue;'>Otech is an awesome place to be!</p>");
+
+document.write('Otech is an awesome place to be!');
---
+++ js-basics/index.js
@@ -0,0 +1,30 @@
+ // 1st js code!
+ console.log("hello world");
+
+ let firstName = "mosh";
+ console.log(firstName);
+
+ const intrestRate = 0.3;
+ intrestRate = 1;
+ console.log(intrestRate);
+
+let name = "mosh"; // string literal
+let age = 30; // number literal
+let isApproved = true; // boolean literal T or False
+let firstName = undefined;
+let lastName = null;
+
+reference types = Object, array, function
+  Object = person, age, name example let person = { name: Blob, age: 4}; (dot notation) person.name = "blob"; console.log(person.name);
+  array = for multiple items   example let color = ['red', 'blue']; color[2] = 'green';  console.log(color); user color.length to console all items
+  function = does things  example function greet(name) { console.log("hello " + name);}   greet('john');  or 
+    function greet(name, lastName) { console.log("hello " + name + '' + lastName);}   greet('john', 'smith');
+    // preform task
+    function square(number) { return number * number;}  square(2);   let number = square(2); console.log(number);
+
+
+primitives = 
+
+
+
+
